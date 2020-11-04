6d918670ef228765b9c18154d300ca158fe3a18b
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2020-11-04 10:59:21 +0900

    Refactoring tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -2,25 +2,24 @@ require './hero'
 
 describe Hero do
 
-  it "has a capitalized name" do
-    hero = Hero.new 'mike'
+  before do
+    @hero = Hero.new 'mike'
+  end
 
-    expect(hero.name).to eq 'Mike' # аналог hero.name == 'Mike', то есть написали имя с маленькой буквы, и ожидаем, что оно будеь с большой.
+  it "has a capitalized name" do
+    expect(@hero.name).to eq 'Mike' # аналог hero.name == 'Mike', то есть написали имя с маленькой буквы, и ожидаем, что оно будеь с большой.
   end
 
   it "can power up" do
-    hero = Hero.new 'mike'
-    expect(hero.power_up).to eq 110
+    expect(@hero.power_up).to eq 110
   end
 
   it "can power down" do
-    hero = Hero.new 'mike'
-    expect(hero.power_down).to eq 90
+    expect(@hero.power_down).to eq 90
   end
 
   it "displays full hero info" do
-    hero = Hero.new 'mike'
-    expect(hero.hero_info).to eq 'Mike has a health of 100'
+    expect(@hero.hero_info).to eq 'Mike has a health of 100'
   end
 
 end
